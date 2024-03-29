56a3574764f4f28bef15965d2e42ee48fc7716ca
adam.baker <abaker82@gmail.com>
2024-03-29 16:53:54 -0400

    added a javasctiprt alert thanking visitors

---
+++ js/app.js
@@ -0,0 +1,9 @@
+        function displayGreeting() {
+       
+            var name = "Adam Baker";
+       
+            alert("Hello, I'm " + name + ". Thank you for visiting my portfolio!");
+       
+        }
+
+        window.onload = displayGreeting;
