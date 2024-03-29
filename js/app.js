cc177bb53b0acc0a8b0d6da1de8d26692e44bef9
adam.baker <abaker82@gmail.com>
2024-03-29 18:55:06 -0400

    comment for loading alert box js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
-        function displayGreeting() {
+//alert message when page loads 
+
+function displayGreeting() {
        
             var name = "Adam Baker";
        
-            alert(`Hello, I'm ${name}. You're obviously highly talented, intelligent and dapper or you wouldnt be here. :)  Thank you for visiting my portfolio!`);
+            alert(`Hello, I'm ${name}. You're obviously highly talented, intelligent and dapper or you wouldnt be here... especially if you are a hiring manager ;)  Thank you for visiting my portfolio!`);
        
         }
 
